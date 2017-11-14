f6f17e9f40a1b0092f6fcc4d300c7d40c03f6d0c
Mohamed HAIDARA <mohamed.haidara-presta@deveryware.com>
2017-11-14 14:53:39 +0100

    bumpversion 1.0.0

--- vagranttoansible/vagranttoansible.py
+++ vagranttoansible/vagranttoansible.py
@@ -15,7 +15,7 @@ import os
 import argparse
 from storm.parsers.ssh_config_parser import ConfigParser
 
-__version__ = "0.1.2"
+__version__ = "1.0.0"
 
 # temporary file to store the ssh configuration
 TMP_SSH_CONF_FILE_NAME = ".vagrant-ssh-config"
